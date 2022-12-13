eb50a6e16fa110c132a8860706a018ed5418abe0
Saxon Stiller <saxon@fastmail.com>
2022-12-13 11:39:10 -0600

    Add simple auth test

--- zoom.test.js
+++ zoom.test.js
@@ -1,14 +1,47 @@
+import { jest } from "@jest/globals";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import Zoom from "./";
 
-test("silly instance test", () => {
-  const client = new Zoom({
-    accountId: "xxxxx",
-    clientId: "xxxxx",
-    clientSecret: "xxxxx",
-  });
-  expect(client).toBeInstanceOf(Zoom);
+const client = new Zoom({
+  accountId: "xxxxx",
+  clientId: "xxxxx",
+  clientSecret: "xxxxx",
 });
 
-test("1 and 1 is 2", () => {
-  expect(1 + 1).toBe(2);
+const server = setupServer(
+  rest.post("https://zoom.us/oauth/token", (_req, res, ctx) => {
+    return res(
+      ctx.json({
+        access_token: "xxxxx",
+        token_type: "bearer",
+        expire_in: 3600,
+        scope: ["admin"],
+      })
+    );
+  }),
+  rest.get("https://api.zoom.us/v2/groups", (_req, res, ctx) => {
+    return res(
+      ctx.json({
+        groups: [
+          { id: "abc", name: "Group 1", total_members: 17 },
+          { id: "def", name: "Group 2", total_members: 4 },
+        ],
+        total_records: 2,
+      })
+    );
+  })
+);
+beforeAll(() => server.listen());
+afterEach(() => {
+  jest.restoreAllMocks();
+  server.resetHandlers();
+});
+afterAll(() => server.close());
+
+test("client auth before data fetch", async () => {
+  const spy = jest.spyOn(client.auth, "request");
+  await client.groups.listGroups();
+
+  expect(spy).toHaveBeenCalled();
 });

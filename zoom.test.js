d565674f5214c72b317d46f126fd78528f92694c
Saxon Stiller <saxon@fastmail.com>
2022-12-13 12:08:58 -0600

    Test auth response caching and auth retries

--- zoom.test.js
+++ zoom.test.js
@@ -33,15 +33,42 @@ const server = setupServer(
   })
 );
 beforeAll(() => server.listen());
-afterEach(() => {
-  jest.restoreAllMocks();
-  server.resetHandlers();
-});
+afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test("client auth before data fetch", async () => {
+test("client auth before request", async () => {
   const spy = jest.spyOn(client.auth, "request");
   await client.groups.listGroups();
 
   expect(spy).toHaveBeenCalled();
+});
+
+test("client auth once for authorized", async () => {
+  const spy = jest.spyOn(client.auth, "request");
+  // List groups twice.
+  await client.groups.listGroups(); // Call auth with authorized response.
+  await client.groups.listGroups(); // Do not call auth.
+
+  expect(spy).toHaveBeenCalledTimes(1);
+});
+
+test("client auth retry after unauthorized", async () => {
+  let reqId = 0;
+  server.use(
+    rest.get("https://api.zoom.us/v2/groups", (_req, res, ctx) => {
+      const status = reqId < 1 ? 401 : 200;
+      reqId++;
+
+      return res(ctx.status(status));
+    })
+  );
+
+  const spy = jest.spyOn(client.auth, "request");
+  // List groups four times.
+  await client.groups.listGroups(); // Call auth with unauthorized response.
+  await client.groups.listGroups(); // Call auth with authorized response.
+  await client.groups.listGroups(); // Do not call auth.
+  await client.groups.listGroups(); // Do not call auth.
+
+  expect(spy).toHaveBeenCalledTimes(2);
 });

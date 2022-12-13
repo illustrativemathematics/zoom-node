12d727e3de9ac8903258e04d584919eac8ef760a
Saxon Stiller <saxon@fastmail.com>
2022-12-13 10:37:34 -0600

    Make sure modules work for testing

--- zoom.test.js
+++ zoom.test.js
@@ -1,3 +1,14 @@
+import Zoom from "./";
+
+test("silly instance test", () => {
+  const client = new Zoom({
+    accountId: "xxxxx",
+    clientId: "xxxxx",
+    clientSecret: "xxxxx",
+  });
+  expect(client).toBeInstanceOf(Zoom);
+});
+
 test("1 and 1 is 2", () => {
   expect(1 + 1).toBe(2);
 });
